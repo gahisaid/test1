fafa136c1d20d13dd0d524315c5d5ae1dc254794
GAHI SAID <said20avril@gmail.com>
2022-10-20 13:08:45 +0000

    Premier version dun projet

---
+++ main.py
@@ -0,0 +1,18 @@
+t=[2,15,3,14,5,1,8,11,4,9]
+j= 0
+tmp = 0
+stable = 1
+while (stable==1) :
+    stable = 0
+    for j in range(0,9):
+        if(t[j] < t[j+1]):
+            tmp = t[j+1]
+            t[j+1] = t[j]
+            t[j] = tmp
+            stable = 1
+
+x=0
+while x<10:
+    print(t[x])
+    x+=1
+
